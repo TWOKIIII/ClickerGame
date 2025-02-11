cc7064cdae80c5ec038203cfc649222136cc342c
Глеб Лятохов <lyatokhov38@gmail.com>
2025-02-11 20:56:45 +0800

    version 2.0 Добавлены 3 бустера

--- ClickerGame/Form1.Designer.cs
+++ ClickerGame/Form1.Designer.cs
@@ -35,7 +35,12 @@
             this.buttonReset = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.buttonInfo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonBoostX2 = new System.Windows.Forms.Button();
+            this.buttonBoostX4 = new System.Windows.Forms.Button();
+            this.buttonAutoClicker = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCharacter)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelScore
@@ -100,11 +105,58 @@
             this.buttonInfo.UseVisualStyleBackColor = true;
             this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonAutoClicker);
+            this.groupBox1.Controls.Add(this.buttonBoostX4);
+            this.groupBox1.Controls.Add(this.buttonBoostX2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(137, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 63);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Бустеры";
+            // 
+            // buttonBoostX2
+            // 
+            this.buttonBoostX2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBoostX2.Location = new System.Drawing.Point(6, 23);
+            this.buttonBoostX2.Name = "buttonBoostX2";
+            this.buttonBoostX2.Size = new System.Drawing.Size(75, 23);
+            this.buttonBoostX2.TabIndex = 0;
+            this.buttonBoostX2.Text = "x2 клик";
+            this.buttonBoostX2.UseVisualStyleBackColor = true;
+            this.buttonBoostX2.Click += new System.EventHandler(this.buttonBoostX2_Click);
+            // 
+            // buttonBoostX4
+            // 
+            this.buttonBoostX4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBoostX4.Location = new System.Drawing.Point(88, 23);
+            this.buttonBoostX4.Name = "buttonBoostX4";
+            this.buttonBoostX4.Size = new System.Drawing.Size(75, 23);
+            this.buttonBoostX4.TabIndex = 1;
+            this.buttonBoostX4.Text = "x4 клик";
+            this.buttonBoostX4.UseVisualStyleBackColor = true;
+            this.buttonBoostX4.Click += new System.EventHandler(this.buttonBoostX4_Click);
+            // 
+            // buttonAutoClicker
+            // 
+            this.buttonAutoClicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAutoClicker.Location = new System.Drawing.Point(169, 23);
+            this.buttonAutoClicker.Name = "buttonAutoClicker";
+            this.buttonAutoClicker.Size = new System.Drawing.Size(75, 23);
+            this.buttonAutoClicker.TabIndex = 2;
+            this.buttonAutoClicker.Text = "Автоклик";
+            this.buttonAutoClicker.UseVisualStyleBackColor = true;
+            this.buttonAutoClicker.Click += new System.EventHandler(this.buttonAutoClicker_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 511);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonInfo);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonReset);
@@ -116,6 +168,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClickerGame";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCharacter)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,6 +182,10 @@
         private System.Windows.Forms.Button buttonReset;
         private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.Button buttonInfo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonAutoClicker;
+        private System.Windows.Forms.Button buttonBoostX4;
+        private System.Windows.Forms.Button buttonBoostX2;
     }
 }
 
--- ClickerGame/Form1.cs
+++ ClickerGame/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ClickerGame
@@ -6,6 +7,8 @@ namespace ClickerGame
     public partial class Form1 : Form
     {
         private int score = 0; // Переменная для хранения счета
+        private int multiplier = 1; // Множитель очков (по умолчанию x1)
+        private Timer autoClickTimer; // Таймер для автокликера
 
         public Form1()
         {
@@ -48,7 +51,7 @@ namespace ClickerGame
 
         private void pictureBoxCharacter_MouseDown(object sender, MouseEventArgs e) // Обработчик нажатия на персонажа
         {
-            score++;
+            score += multiplier; // Добавляем очки с учетом множителя
             labelScore.Text = "" + score;
 
             if (score >= 50) // Если счет достиг 50, открываем окно победы
@@ -56,6 +59,46 @@ namespace ClickerGame
                 WinForm winForm = new WinForm(this);
                 winForm.ShowDialog();
             }
+        }
+
+        private void buttonBoostX2_Click(object sender, EventArgs e)
+        {
+            multiplier = 2;
+            buttonBoostX2.Enabled = false; // Отключаем кнопку, чтобы нельзя было активировать снова
+
+            Task.Delay(10000).ContinueWith(_ =>
+            {
+                multiplier = 1;
+                buttonBoostX2.Invoke((MethodInvoker)(() => buttonBoostX2.Enabled = true)); // Включаем кнопку обратно
+            });
+        }
+
+        private void buttonBoostX4_Click(object sender, EventArgs e)
+        {
+            multiplier = 4;
+            buttonBoostX4.Enabled = false; // Отключаем кнопку
+
+            Task.Delay(5000).ContinueWith(_ =>
+            {
+                multiplier = 1;
+                buttonBoostX4.Invoke((MethodInvoker)(() => buttonBoostX4.Enabled = true)); // Включаем кнопку обратно
+            });
+        }
+
+        private void buttonAutoClicker_Click(object sender, EventArgs e)
+        {
+            buttonAutoClicker.Enabled = false; // Отключаем кнопку
+            autoClickTimer = new Timer();
+            autoClickTimer.Interval = 500; // Каждые 500 мс +1 клик
+            autoClickTimer.Tick += (s, ev) => pictureBoxCharacter_MouseDown(null, null);
+            autoClickTimer.Start();
+
+            Task.Delay(5000).ContinueWith(_ =>
+            {
+                autoClickTimer.Stop();
+                autoClickTimer.Dispose();
+                buttonAutoClicker.Invoke((MethodInvoker)(() => buttonAutoClicker.Enabled = true)); // Включаем кнопку обратно
+            });
         }
     }
 }
